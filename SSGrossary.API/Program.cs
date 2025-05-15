5c44054f8761d94ef6065c68fb49442fe8415b28
Anil Dhakad <anildhakad8587@gmail.com>
2025-05-15 09:36:01 +0530

    Add project files.

---
+++ SSGrossary.API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
