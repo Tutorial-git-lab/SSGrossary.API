d7a18a9763e028a0a86142971944e85ae0b34242
Anil Dhakad <anildhakad8587@gmail.com>
2025-05-23 13:01:34 +0530

    Adding DTO

---
+++ SSGrossary.API/DTO/Request/DistrictRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SSGrossary.API.DTO.Request
+{
+    public class DistrictRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int StateId {  get; set; }
+
+        public int CountryId {  get; set; }
+    }
+}
---
+++ SSGrossary.API/DTO/Request/StateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SSGrossary.API.DTO.Request
+{
+    public class StateRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CountryId {  get; set; }
+    }
+}
